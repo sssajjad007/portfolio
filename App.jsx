75294f90e82b9bed15ac7ee35d9bee6dd9ef3a16
sssajjad007 <sssajjad.76@gmail.com>
2023-12-29 23:43:11 +0330

    init

--- App.jsx
+++ App.jsx
@@ -241,7 +241,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    backgroundColor: '#0E1117',
+    backgroundColor: 'red',
   },
   SafeAreaViewStyle: {
     flex: 1,
