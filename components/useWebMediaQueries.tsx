f7d5bbf4c21f12cb3005d69478589d12937da962
sssajjad007 <sssajjad.76@gmail.com>
2024-01-12 00:03:40 +0330

    feat: create media query hook

---
+++ components/useWebMediaQueries.tsx
@@ -0,0 +1,10 @@
+import { useMediaQuery } from "react-responsive";
+
+export function useWebMediaQueries() {
+  const isDesktop = useMediaQuery({ minWidth: 1300 });
+  const isTablet = useMediaQuery({ minWidth: 800, maxWidth: 1300 - 1 });
+  const isMobile = useMediaQuery({ maxWidth: 800 - 1 });
+  const isTabletOrMobile = isMobile || isTablet;
+  const isTabletOrDesktop = isDesktop || isTablet;
+  return { isMobile, isTablet, isTabletOrMobile, isTabletOrDesktop, isDesktop };
+}
